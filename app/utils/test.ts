88c3368fa3faf13bffc7101fe22006b22f617da0
lef237 <lef237k5@gmail.com>
2025-02-07 17:38:06 +0900

    Rename index.ts to test.ts and update import path for todosTable

--- app/utils/test.ts
+++ app/utils/test.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { eq } from "drizzle-orm";
-import { todosTable } from "./db/schema";
+import { todosTable } from "~/db/schema";
 
 const db = drizzle(process.env.DATABASE_URL!);
 
