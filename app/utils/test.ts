ce79130e3308799c89901a4dbb5c43ffe4fe92ca
lef237 <lef237k5@gmail.com>
2025-02-07 20:12:49 +0900

    Refactor app/utils/test.ts

--- app/utils/test.ts
+++ app/utils/test.ts
@@ -1,9 +1,7 @@
 import "dotenv/config";
-import { drizzle } from "drizzle-orm/node-postgres";
 import { eq } from "drizzle-orm";
 import { todosTable } from "~/db/schema";
-
-const db = drizzle(process.env.DATABASE_URL!);
+import { db } from "~/db/drizzleConnect";
 
 async function main() {
   // INSERT
